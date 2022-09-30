41d05c6c707f55bf7d7757596ef399ca30e22d43
Carlos Eduardo <cadu4duarte@gmail.com>
2022-09-30 13:31:56 -0300

    feat: edit import pages

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from "@expo-google-fonts/inter";
 import { Home } from './src/screens/Home';
 import { Background } from "./src/components/Background";
-import { Loading } from './src/screens/Loading';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
