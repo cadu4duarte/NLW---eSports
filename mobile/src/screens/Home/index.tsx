e617c591e182a9f9e62cad7a94177a2f9c037ff6
Carlos Eduardo <cadu4duarte@gmail.com>
2022-09-30 13:33:22 -0300

    style: edit layout

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,10 @@
-import {View, Image} from "react-native";
+import {View, Image, FlatList} from "react-native";
 import { styles } from "./styles";
 import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+import {GAMES} from "../../utils/games";
+import { GameCard } from "../../components/GameCard";
+import React from "react";
 
 export function Home() {
   return(
@@ -8,7 +12,27 @@ export function Home() {
       <Image 
         source={logoImg}
         style={styles.logo}
-      /> 
+      />
+
+      <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList 
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <GameCard 
+            data={item}
+          /> 
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
+
+      
     </View>
   );
 }
