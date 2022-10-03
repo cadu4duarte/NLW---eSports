17118884ecbae04b4ebc3db3e69059bd2431fd8b
Carlos Eduardo <cadu4duarte@gmail.com>
2022-10-03 19:55:37 -0300

    feat: create database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,20 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+
+app.post("/ads", (request, response) => {
+  return response.json([]);
+});
+
+
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     {id: 1, name: "anúncio 1"},
     {id: 2, name: "anúncio 2"},
@@ -10,5 +22,11 @@ app.get("/ads", (request, response) => {
     {id: 4, name: "anúncio 4"},
   ]);
 });
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([]);
+})
 
 app.listen(3333);
