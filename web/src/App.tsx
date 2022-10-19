6b8989c6930315ba4b11485e5524c766e13b134b
Carlos Eduardo <cadu4duarte@gmail.com>
2022-10-19 17:09:28 -0300

    style: edit layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -86,6 +86,51 @@ function App() {
               <div className="flex gap-6">
                 <div className="flex flex-col gap-2">
                   <label htmlFor="weekDays">Quando costuma jogar?</label>
+
+                  <div className="grid grid-cols-4 gap-2">
+                    <button 
+                      title="Domingo"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      D
+                    </button>
+                    <button 
+                      title="Segunda"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      S
+                    </button>
+                    <button 
+                      title="Terça"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      T
+                    </button>
+                    <button 
+                      title="Quarta"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      Q
+                      </button>
+                    <button 
+                      title="Quinta"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      Q
+                    </button>
+                    <button 
+                      title="Sexta"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      S
+                    </button>
+                    <button 
+                      title="Sábado"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      S
+                      </button>
+                  </div>
                 </div>
 
                 <div className="flex flex-col gap-2 flex-1">
@@ -99,15 +144,23 @@ function App() {
                 
               </div>
 
-              <div>
+              <div className="mt-2 flex gap-2 text-sm">
                 <Input type="checkbox" />
                 Costumo me conectar ao chat de voz
               </div>
 
-              <footer>
-                <button>Cancelar</button>
-                <button type="submit">
-                  <GameController />
+              <footer className="mt-4 flex justify-end gap-4">
+                <Dialog.Close
+                  type="button" 
+                  className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600"
+                >
+                  Cancelar
+                </Dialog.Close>
+                <button 
+                  type="submit"
+                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+                >
+                  <GameController className="w-6 h-6" />
                   Encontrar duo
                 </button>
               </footer>
